2f752e8eadd4888815a932782c0bc52f13269e5e
Michael Romay <amerror@gmail.com>
2020-07-27 18:18:44 -0700

    created conditionary styling for messages

--- src/components/Messages/Messages.jsx
+++ src/components/Messages/Messages.jsx
@@ -3,33 +3,59 @@ import { messages, moreMessages } from '../../data/data';
 import MessagesList from '../MessagesList/MessagesList';
 import AwesomeSlider from 'react-awesome-slider';
 import AwesomeSliderStyles from '../../styles/styles.scss';
+import styles from './Messages.css';
 
 const Messages = () => {
+  const titleTop = messages[0].section_title;
+  const titleBottom = moreMessages[0].section_title;
+
+  const sectionTitleTop = messages[0].section_title;
+  const sectionTitleBottom = moreMessages[0].section_title;
+
   const messageNodes = messages[0].quotes.map((data) => {
     return (
       <div key={data.attribution}>
-        <MessagesList key={data.attribution} {...data} />
+        <MessagesList
+          key={data.attribution}
+          {...data}
+          sectionTitle={sectionTitleTop}
+        />
       </div>
     );
   });
+
   const moreMessageNodes = moreMessages[0].quotes.map((data) => {
     return (
       <div key={data.attribution}>
-        <MessagesList key={data.attribution} {...data} />
+        <MessagesList
+          key={data.attribution}
+          {...data}
+          sectionTitle={sectionTitleBottom}
+        />
       </div>
     );
   });
   return (
     <>
       <section>
-        <h1>Messages</h1>
-        <AwesomeSlider scssModule={AwesomeSliderStyles}>
-          {messageNodes}
-        </AwesomeSlider>
-
-        <AwesomeSlider scssModule={AwesomeSliderStyles}>
-          {moreMessageNodes}
-        </AwesomeSlider>
+        <section className={styles.Top}>
+          <h1 className={styles.Title}>{titleTop}</h1>
+          <AwesomeSlider
+            className={styles.Slider}
+            scssModule={AwesomeSliderStyles}
+          >
+            {messageNodes}
+          </AwesomeSlider>
+        </section>
+        <section className={styles.Bottom}>
+          <h1 className={styles.Title}>{titleBottom}</h1>
+          <AwesomeSlider
+            className={styles.Slider}
+            scssModule={AwesomeSliderStyles}
+          >
+            {moreMessageNodes}
+          </AwesomeSlider>
+        </section>
       </section>
     </>
   );
--- src/components/MessagesList/MessagesList.jsx
+++ src/components/MessagesList/MessagesList.jsx
@@ -1,10 +1,15 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import styles from './MessagesList.css';
 
-const MessagesList = ({ message, attribution }) => {
+const MessagesList = ({ message, attribution, sectionTitle }) => {
+  const messageTop = <h3 className={styles.TopMessage}>{message}</h3>;
+  const messageBottom = <h3 className={styles.BottomMessage}>{message}</h3>;
+
   return (
-    <div>
-      <h3>{message}</h3>
+    <div className={styles.MessagesList}>
+      {sectionTitle === 'Congratulatory Messages' && messageTop}
+      {sectionTitle === 'More Messages!' && messageBottom}
       <h4>{attribution}</h4>
     </div>
   );
@@ -13,6 +18,7 @@ const MessagesList = ({ message, attribution }) => {
 MessagesList.propTypes = {
   message: PropTypes.string,
   attribution: PropTypes.string,
+  sectionTitle: PropTypes.string,
 };
 
 export default MessagesList;
